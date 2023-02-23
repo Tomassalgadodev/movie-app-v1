f8cabab94639bed1b2446cddee0b1395d469d60c
Tomas Salgado <tomassalgadodev@gmail.com>
2023-02-23 16:25:53 -0700

    component: MovieContainer component

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import movieData from '../movie-data';
 import Header from '../Header/Header';
 import MovieDetails from '../MovieDetails/MovieDetails';
+import MovieContainer from '../MovieContainer/MovieContainer';
 import './App.css';
 
 class App extends Component {
@@ -18,6 +19,7 @@ class App extends Component {
             <main style={{backgroundImage: `linear-gradient(to right, #000, #000 10%, rgb(0, 0, 0, .8) 35%, rgb(0, 0, 0, .7) 40%, rgb(0, 0, 0, 0.1) 60%), url(${this.state.currentMovie.backdrop_path})`}}>
                 <Header />
                 <MovieDetails />
+                <MovieContainer />
             </main>
         )
     }
---
+++ src/MovieContainer/MovieContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './MovieContainer.css'
+
+const MovieContainer = () => {
+    return (
+        <React.Fragment>
+            <h3 className='movie-container-heading'>Popular on Movie App</h3>
+            <div className='movie-container'>
+
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default MovieContainer;
