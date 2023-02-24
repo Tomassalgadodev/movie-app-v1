f50079a5d00cd45173e7f7f7b867e6c049b43eec
Tomas Salgado <tomassalgadodev@gmail.com>
2023-02-24 13:57:28 -0700

    feat: Display release budget and length with appropriate pre and suffixes

--- src/DetailsContainer/DetailsContainer.js
+++ src/DetailsContainer/DetailsContainer.js
@@ -4,13 +4,16 @@ import './DetailsContainer.css';
 
 const DetailsContainer = ({ score, release, budget, length }) => {
 
+    let [year, month, day] = release.split('-');
+    release = [month, day, year].join('/')
+
     return (
         <div className='details-container'>
             <DetailCard type='SCORE' value={score} />
             <DetailCard type='RATING' value='R' />
             <DetailCard type='RELEASE' value={release} />
-            <DetailCard type='BUDGET' value={budget} />
-            <DetailCard type='LENGTH' value={length} />
+            <DetailCard type='BUDGET' value={`$${budget}`} />
+            <DetailCard type='LENGTH' value={`${length} min`} />
         </div>
     )
 }
