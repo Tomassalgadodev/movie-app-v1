eebead239e24c2049b1b6d0cb5e972c1976dac3f
Tomas Salgado <tomassalgadodev@gmail.com>
2023-02-24 12:25:56 -0700

    feat: Display runtime in hours and minutes on main screen

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -5,7 +5,8 @@ const MovieDetails = ({ title, releaseDate, rating, tagline, genres, runtime }) 
 
     const releaseYear = releaseDate.substring(0, 4);
     const averageRating = rating % 1 ? rating.toFixed(1) : rating;
-    genres = genres.join(', ')
+    genres = genres.join(', ');
+    runtime = runtime < 60 ? `${runtime}m` : `${Math.floor(runtime / 60)}h ${runtime % 60}m`
 
 
     return (
@@ -13,7 +14,7 @@ const MovieDetails = ({ title, releaseDate, rating, tagline, genres, runtime }) 
             <h2 className='movie-title'>{title}</h2>
             <div className='details-subheading'>
                 <p>{'<IMDB>'} {averageRating}/10</p>
-                <p className='details'>{releaseYear} • {genres} • {runtime}m</p>
+                <p className='details'>{releaseYear} • {genres} • {runtime}</p>
             </div>
             <p className='tagline'>{tagline}</p>
             <button>LEARN MORE</button>
